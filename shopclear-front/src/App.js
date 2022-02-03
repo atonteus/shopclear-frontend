379e333dba4421ae8ffe4c79f2d043b420b0e4b2
Antonio Mateus <94945184+atonteus@users.noreply.github.com>
2022-02-02 23:13:41 -0300

    Update App.js

--- shopclear-front/src/App.js
+++ shopclear-front/src/App.js
@@ -8,7 +8,7 @@ const App = () => (
     <div className="App">
       <div className="gradient__main">
         <Navbar/>
-        <Navbar/>
+        
         {/* <Header/> */}
       </div>
       {/* <Article/> */}
