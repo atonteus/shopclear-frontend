78a1c77c8c988d79120982c176ac82d6946973e7
Antonio Mateus <94945184+atonteus@users.noreply.github.com>
2022-02-02 23:22:44 -0300

    Update Navbar.jsx

--- shopclear-front/src/components/navbar/Navbar.jsx
+++ shopclear-front/src/components/navbar/Navbar.jsx
@@ -1,22 +1,49 @@
-import React from "react";
-import logo from "../../assets/shopclear.svg";
-import "./navbar.css";
+import React, { useState } from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import logo from '../../assets/shopclear.svg'; 
+import './navbar.css';
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <div className="shop__navbar">
       <div className="shop__navbar-links">
-        <div ClassName="shop__navbar-logo">
-        <img src={logo} alt="logo" />
+        <div className="shop__navbar-links_logo">
+          <img src={logo} alt=''/>
         </div>
-        <div ClassName="shop__navbar-links_container">
+        <div className="shop__navbar-links_container">
            <p><a href="#home">Home</a></p>
            <p><a href="#cases">Cases</a></p>
            <p><a href="#about">About</a></p>
            <p><a href="#article">Article</a></p>
         </div>
       </div>
+      <div className="shop__navbar-sign">
+        <p>Entrar</p>
+        <button type="button">Cadastre-se</button>
+      </div>
+      <div className="shop__navbar-menu">
+        {toggleMenu
+          ? <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+          : <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(true)} />}
+        {toggleMenu && (
+        <div className="shop__navbar-menu_container scale-up-center">
+          <div className="shop__navbar-menu_container-links">
+           <p><a href="#home">Home</a></p>
+           <p><a href="#cases">Cases</a></p>
+           <p><a href="#about">About</a></p>
+           <p><a href="#article">Article</a></p>
+          </div>
+          <div className="shop__navbar-menu_container-links-sign">
+            <p>Entrar</p>
+            <button type="button">Cadastre-se</button>
+          </div>
+        </div>
+        )}
+      </div>
     </div>
   );
 };
+
 export default Navbar;
