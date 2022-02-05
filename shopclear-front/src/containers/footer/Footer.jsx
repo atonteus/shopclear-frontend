73ef196ebe9dcf41b5858f933852b42e12703bfb
Antonio Mateus <94945184+atonteus@users.noreply.github.com>
2022-02-04 23:57:46 -0300

    Add files via upload
    initial Footer component

---
+++ shopclear-front/src/containers/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import ShopLogo from '../../logo.svg';
+import './footer.css';
+
+const Footer = () => (
+  <div className="Shop__footer section__padding">
+    <div className="Shop__footer-heading">
+      <h1 className="gradient__text">More info</h1>
+    </div>
+  </div>
+);
+
+export default Footer;
