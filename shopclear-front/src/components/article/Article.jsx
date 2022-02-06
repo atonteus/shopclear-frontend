1d866e2a94d1ba0c15c5ccd0c38fdcc5498b4497
Antonio Mateus <94945184+atonteus@users.noreply.github.com>
2022-02-06 02:21:31 -0300

    Update Article.jsx
    initial Article component

--- shopclear-front/src/components/article/Article.jsx
+++ shopclear-front/src/components/article/Article.jsx
@@ -1,3 +1,19 @@
 import React from 'react';
 import './article.css';
 
+const Article = ({ imgUrl, date, text }) => (
+  <div className="shop__blog-container_article">
+    <div className="shop__blog-container_article-image">
+      <img src={imgUrl} alt="blog_image" />
+    </div>
+    <div className="shop__blog-container_article-content">
+      <div>
+        <p>{date}</p>
+        <h3>{text}</h3>
+      </div>
+      <p>Saiba mais</p>
+    </div>
+  </div>
+);
+
+export default Article;
