c2af755700d8d9d253be85c31cf4c6169b2365f9
Antonio Mateus <94945184+atonteus@users.noreply.github.com>
2022-02-06 02:24:09 -0300

    Add files via upload
    Initial Sign up component

---
+++ shopclear-front/src/components/signup/Signup.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './signup.css';
+
+const Signup = () => (
+  <div className="shop__signup">
+    <div className="shop__signup-content">
+      <p>Realize o cadastro de forma rápida e segura</p>
+      <h3>Registre-se agora e um de nossos consultores irá entrar em contato para iniciar o seu projeto! </h3>
+    </div>
+    <div className="shop__signup-btn">
+      <button type="button">Começar agora!</button>
+    </div>
+  </div>
+);
+
+export default Signup;
