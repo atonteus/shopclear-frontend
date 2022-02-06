6134cbd527007e71b2f1dd23870cda7567cdee46
Antonio Mateus <94945184+atonteus@users.noreply.github.com>
2022-02-05 22:55:33 -0300

    Update App.js

--- shopclear-front/src/App.js
+++ shopclear-front/src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import {Navbar} from './components'
-import {Header} from '/containers'
+import {Navbar} from './components';
+import {Header} from './containers';
 import './App.css';
 
 const App = () => (
 
     <div className="App">
       <div className="gradient__main">
-        <Navbar/>
-        
-        {/* <Header/> */}
+      <Navbar/>
       </div>
+      
       <Header/>
       {/* <Article/> */}
       {/* <Footer/> */}
